997cfde2f812ca066397db66a98af1a2efa29909
schaumlk <schaumlk@oregonstate.edu>
2023-04-15 19:46:41 -0700

    Update remove handler

--- src/components/addDrug.js
+++ src/components/addDrug.js
@@ -1,10 +1,21 @@
-import React from "react";
-import { Form, Input } from "semantic-ui-react";
+import {React, useState} from "react";
+import { Component } from "react";
+import { useParams } from 'react-router-dom';
+import { Dropdown, Form, Input } from "semantic-ui-react";
 import api from "../services/api";
+import axios from 'axios';
+import { getValue } from "@testing-library/user-event/dist/utils";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+var drugId2=0;
+var doseId2=0;
+
+var drugIdState=0 ;
+var dosageIdState=0;
 
 // Adds a new drug to the table of a given animal
-class addDrug extends React.Component {
-
+class AddDrug extends Component {
+  
     state = {
         name: "",
         method: "",
@@ -13,32 +24,216 @@ class addDrug extends React.Component {
         doseLow: "",
         doseHigh: "",
         dosageUnit: "",
-        notes: ""
-    };
-    setState=this.setState.bind(this)
-
-    add = (e) => {
-        e.preventDefault();
+        notes: "",
+        drugIdState: 0,
+        doseUnitId:0,
+        unitId: 0,
+        options:0, 
+        methodUnitId: 0,       
         
-        if (this.state.name === "") {
-            alert("Drug field is mandatory!");
-            return;
-        }
+    };
+
+    setState=this.setState.bind(this);
+    
+    handleDropdownChangeConc = (e, { value }) => {
+
+        this.setState({ concentrationUnit: value });
+      // alert(this.state.unitOptions.find(o => o.value ==value)?.key);
+      this.setState({unitId: value });
+      this.setState({options: value})
+      //  alert(value);
+      };
+
+      handleDropdownChangeDose = (e, { value }) => {
+        // this.setState({ dosageUnit: value });
+        this.setState({optionDose: value})
+        //  alert("dosage unity :"+value)
+      };
+
+      handleDropdownChangeMethod = (e, { value }) => {
+        this.setState({ methodUnit: value });
+        //alert(value)  
+
+        this.setState({methodUnitId: value });
+        // this.setState({optionMethod: value})
+        // alert(value)
+      };
+      
+
+      
+      
+    add = (e) => {
+
+      var concentrationValue= this.state.concentration;
+      var unitIdValue = this.state.unitId;
+      var methodUnitId = this.state.methodUnitId
+      
+        e.preventDefault();
+        // alert(this.state.name)
         api.post('/drugs', {
-            drug_id: 'drug_id',
+            Authentication: API_KEY,
+            //drug_id: 161,
             name: this.state.name
+            
           })
-          .then(function (response) {
-            console.log(response);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
+          .then(
+            api.get('/drugs')
+            .then(response => {
+              const myPromise = new Promise((resolve, reject) => {
+                try {
+                  const drugId = Object.values(response.data);
+              
+                  // drugId2 = drugId.pop().drug_id;
+                  //this.setState({drugIdState: drugId.pop().drug_id });  
+                  drugIdState = drugId.pop().drug_id ;
+                  console.log("the drug list:");    
+                  console.log(drugId.pop()) ;
+                  // const result = myClassInstance.myMethod(); // Execute the method
+              
+                  resolve("ok"); // Resolve the Promise with the result
+                } catch (error) {
+                  reject(error); // Reject the Promise if an error occurs
+                }
+
+              }); 
+
+            myPromise.then(response => {
+                    api.post('/dosages', {
+                      Authentication: API_KEY,
+                      animal_id: localStorage.getItem("animalId"),       // Needs to come from dropdown list of /animals!
+                      drug_id: drugIdState,         // Needs to come from get methods of /drugs!
+                      dose_low: this.state.doseLow,
+                      dose_high: this.state.doseHigh,
+                      dose_unit_id: this.state.optionDose,  // Needs to come from /units!
+                      notes: this.state.notes,
+                    })
+                    .then(function (response) {
+                      
+                      api.get('/dosages')
+                      .then(response => {
+                        const promiseC = new Promise((resolve, reject) => {
+                          try {
+                            const drugId = Object.values(response.data);                       
+                            
+                            const dosageId = Object.values(response.data);
+                            dosageIdState = dosageId.pop().dosage_id; 
+                           // doseId2 = dosageId.pop().dosage_id;                            
+                        
+                            resolve("ok"); // Resolve the Promise with the result
+                          } 
+                          
+                          catch (error) {
+                            reject(error); // Reject the Promise if an error occurs
+                          }
+          
+                        }); 
+                        
+                      alert("the id of the dosage is:"+concentrationValue);
+                        promiseC.then(response => {
+
+                          
+                          // console.log("the id of the dosage is:"+dosageIdState);
+                          // console.log("the id of the dosage is:"+dosageIdState);
+                            api.post('/concentrations', {
+            
+                              Authentication: API_KEY,
+                              value: concentrationValue,
+                              unit_id: unitIdValue,
+                              dosage_id: dosageIdState,             // Needs to come from get from /dosage!
+                            })
+
+                              
+        
+                          api.post('/delivery', {
+                            Authentication: API_KEY,
+                            //drug_id: 161,
+                            dosage_id: dosageIdState,
+                            method_id: methodUnitId,
+                            
+                          })
+                          .then(
+                          )
+                  
+                            .catch(error => {
+                              // console.error("Error: cannot receive drug data from DB")
+                              console.error(error)
+                            });
+                                            
+                   
+                      } )                      
+                      .catch(function (error) {
+                        console.log("the error PROM. is:"+error);
+                      });
+                    })
+          
+              
+              })
+              .catch(function (error) {
+                console.log("the error is:"+error.response.data.message);
+              });
+   
+            });
+
+            
+             })
+  
+            .catch(error => {
+              // console.error("Error: cannot receive drug data from DB")
+              // console.error(error)
+            })
+
+          );
+       
+   
+          
+        // // //return; 
+        // // if (this.state.name === "") {
+        // //     alert("Drug field is mandatory!");
+        // //     return;
+        // // }
+        
         this.props.addDrugHandler(this.state);
         this.setState({ id: "", name: "", method: "", concentration: "", concentrationUnit: "", doseLow: "", doseHigh: "", dosageUnit: "", notes: "" });
     };
+
+    // Retrieve data from DB using Axios
+    componentDidMount() {
+        // Get unit information
+        api.get('/units')
+          .then(response => {
+            const unitOptions = response.data.map(unit => ({
+                key: unit.unit_id,
+                text: unit.name,
+                value: unit.unit_id
+            }
+            ))
+            this.setState({unitOptions});
+          })
+          .catch(error => {
+            console.error("Error: cannot receive drug data from DB")
+          });
+          
+
+        // Get method information
+        api.get('/methods')
+          .then(response => {
+            const methodOptions = response.data.map(method => ({
+                key: method.method_id,
+                text: method.name,
+                value: method.method_id
+            }
+            ))
+            this.setState({methodOptions});
+          })
+          .catch(error => {
+            console.error("Error: cannot receive drug data from DB")
+          });
+          
+      }
+
     render() {
         return (
+
             <div className="ui main">
                 <h2> Add Drug</h2>
                 <form className="ui form" onSubmit={this.add}>
@@ -54,14 +249,23 @@ class addDrug extends React.Component {
                     />
                     
                     <Form.Field
-                        control={Input}
-                        name="method"
-                        label="Method"
-                        placeholder="Method"
-                        className="right aligned"
-                        value={this.state.method}
-                        onChange={(e) => this.setState({ method: e.target.value })}
-                    />
+                      control={Dropdown}
+                      name="methodUnit"
+                      label="Method Unit"
+                      placeholder="IV"
+                      selection
+                      options={this.state.methodOptions || []}
+                      value={this.state.methodUnitId}
+                      onChange={this.handleDropdownChangeMethod}
+                />
+                    {/* //     control={Input}
+                    //     name="method"
+                    //     label="Method"
+                    //     placeholder="Method"
+                    //     className="right aligned"
+                    //     value={this.state.method}
+                    //     onChange={(e) => this.setState({ method: e.target.value })}
+                    //  */}
                     </Form.Group>
 
                     <Form.Group>
@@ -76,17 +280,14 @@ class addDrug extends React.Component {
                         onChange={(e) => this.setState({ concentration: e.target.value })}
                     />   
                     <Form.Field
-                        control={Input}
-                        options={[
-                                      { key: 'mg/kg', text: 'mg/kg', value: 'mg/kg' },
-                                      { key: 'µg/kg', text: 'µg/kg', value: 'µg/kg' },
-                                      { key: 'ml/kg', text: 'ml/kg', value: 'ml/kg' },
-                                    ]}
+                        control={Dropdown}
                         name="concentrationUnit"
                         label="Concentration Unit"
                         placeholder="mg/kg"
+                        selection
+                        options={this.state.unitOptions || []}
                         value={this.state.options}
-                        onChange={(e) => this.setState({ concentrationUnit: e.target.value })}
+                        onChange={this.handleDropdownChangeConc}
                     />
                     </Form.Group>
 
@@ -110,12 +311,14 @@ class addDrug extends React.Component {
                         value={this.state.doseHigh}
                         onChange={(e) => this.setState({ doseHigh: e.target.value })}/>     
                     <Form.Field
-                        control={Input} 
-                        name="doseUnit" 
+                        control={Dropdown} 
+                        name="dosageUnit" 
                         label="Dosage Unit"
                         placeholder="Dosage Unit"
-                        value={this.state.doseUnit}
-                        onChange={(e) => this.setState({ doseUnit: e.target.value })}/>
+                        selection
+                        options={this.state.unitOptions || []}
+                        value={this.state.optionDose}
+                        onChange={this.handleDropdownChangeDose}/>
                     </Form.Group>
                     
                     <Form.Field
@@ -134,4 +337,4 @@ class addDrug extends React.Component {
     }
 }
 
-export default addDrug;
+export default AddDrug;
--- src/components/animalList.js
+++ src/components/animalList.js
@@ -1,24 +1,84 @@
-import _ from 'lodash'
 import React, { Component } from 'react'
 import { Dropdown } from 'semantic-ui-react'
+import get from '../services/get';
+import api from '../services/api';
+const animalUri = "https://vaddb.liamgombart.com/animals";
+// const animalUri = `${process.env.REACT_APP_TLD}/animals`
 
 // Creates the dropdown component on the homepage for the user to select which animal to view.
-const animals = ["", "Camelid", "Cat", "Cattle", "Dog", "Equine", "Goat/Sheep", "Swine" ];
-const animalOptions = _.map(animals, (animal, index) => ({
-  key: animals[index],
-  text: animal,
-  value: animals[index],
-}))
 
 export default class DropdownAnimalSearchQuery extends Component {
-  state = { searchQuery: '' }
+  state = { 
+    searchQuery: '',
+    selectOptions: [],
+  }
+
+
+  componentDidMount() {
+    this.setOptions();
+  }
+
+  setOptions = async () => {
+    try {
+      const response = await get(animalUri);
+      const options = this.mapOptions(response);
+
+      this.setState({
+        ...this.state,
+        selectOptions: options,
+      })
+
+    } catch (e) {
+      this.setState({
+        ...this.state,
+        selectOptions: [],
+      })
+    
+    }
+  }
+
+  mapOptions = (animals) => {
+    const options = animals.map(animal => ({
+        key: animal.id,
+        text: animal.name,
+        value: animal.name,
+    }));
+
+    return options
+  }
 
   handleChange = (e, { searchQuery, value }) =>{
-    if (value === 'Goat/Sheep') {
-      value = 'goat_sheep'
-    }
-    window.location.href = `/drugs/${value}`
-    this.setState({ searchQuery, value })
+    const encodedValue = encodeURIComponent(value);
+    
+
+    api.get('animals')
+            .then(response => {
+              
+              const selectedOptionKey =0;
+            const animals = Object.values(response.data);
+            animals.forEach(element => {
+           console.log(value);
+              if (element.name===value){
+                    alert("the element is:"+ element.animal_id);
+                    localStorage.setItem("animalId", element.animal_id);
+              }
+            });
+            // selectedOptionKey = animals.find((option) => option.animal_id === value);
+       //alert(selectedOptionKey);
+      //console.log(animals)
+    })
+    .catch(error => {
+              // console.error("Error: cannot receive drug data from DB")
+               console.error(error)
+            })
+            
+      //  console.log(this.state.selectOptions.find(o => o.value ==value)?.key);
+alert(encodedValue)
+    window.location.href = `/drugs/${encodedValue}`;
+    https://vaddb.liamgombart.com/dosages?animal_id=undefined
+    
+
+    this.setState({ ...this.state, searchQuery: value });
   }   
 
   handleSearchChange = (e, { searchQuery }) => this.setState({ searchQuery })
@@ -31,7 +91,7 @@ export default class DropdownAnimalSearchQuery extends Component {
         fluid
         onChange={this.handleChange}
         onSearchChange={this.handleSearchChange}
-        options={animalOptions}
+        options={this.state.selectOptions}
         placeholder='Animal'
         search
         searchQuery={searchQuery}
